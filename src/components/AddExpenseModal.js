bf3c9646dfb3e3f1b697fb120f804a426e663ba6
Jonatan Frencia <frenciaj@gmail.com>
2025-12-04 17:48:12 -0300

    Create AddExpenseModal.js

---
+++ src/components/AddExpenseModal.js
@@ -0,0 +1,2 @@
+// Modal con inputs: Descripción, Monto, Moneda, % Inflación, Mes Inicial
+// Al confirmar: for (let m = 0; m < 12; m++) { montoAjustado = monto * (1 + infl/100)**m; addGasto({ mes: mesesInicial + m, montoUsd: ... }) }
