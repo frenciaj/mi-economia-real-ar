aec38158381eb743028ff83a1414f33bbd4af2df
Jonatan Frencia <frenciaj@gmail.com>
2025-12-04 17:32:43 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        emerald: {
+          600: '#059669',
+        }
+      }
+    },
   },
   plugins: [],
+  darkMode: 'class',
 }
